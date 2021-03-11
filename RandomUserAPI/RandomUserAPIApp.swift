586d9ac682ce66f0e60c97b353cbd249340de588
Seokhyun Kim <wjapp72@gmail.com>
2021-03-11 09:48:02 -0800

    Initial Commit

---
+++ RandomUserAPI/RandomUserAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserAPIApp.swift
+//  RandomUserAPI
+//
+//  Created by Seokhyun Kim on 2021-03-11.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
